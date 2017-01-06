a1fb5cf68d61ef30fb1865da7195df2fb2197abf
rick3545 <rgeesaman@northernpolarbears.com>
2017-01-06 17:50:49 -0500

    Revert "Revert "Revert "Change"""
    This reverts commit 483f8f7ae73dc42023b63f97965b91f7abb9a38a.

--- example0.py
+++ example0.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'E0'
-strategy_name = 'Collude.'
+strategy_name = 'Collude'
 strategy_description = 'Always collude.'
     
 def move(my_history, their_history, my_score, their_score):
